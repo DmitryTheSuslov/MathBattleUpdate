6d22e9471f5560941280bc3b15bbe47107a9d83c
Димка <mityka2004@yandex.ru>
2020-07-12 23:55:12 +0300

    Подготовка к heroku

---
+++ api.py
@@ -0,0 +1,5 @@
+import tasks_resource, users_resource
+from flask_restful import Api
+from app import app
+
+api = Api(app)
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+import tasks_resource, users_resource
+from flask_restful import Api
+import os
+from data import db_session
+
+db_session.global_init('db/DataBase.sqlite')
+app = Flask(__name__)
+
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=port)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('MathBattleMainPage.html', style_way=url_for('static', filename='css/styles.css'))
+
+
+if __name__ == '__main__':
+    from api import *
+
+
+    session = db_session.create_session()
+    api.add_resource(users_resource.UserResource, '/api/users/<user_login>')
+    api.add_resource(users_resource.UserListResource, '/api/user/')
+    api.add_resource(tasks_resource.TaskResource, '/api/task/<task_id>')
+    api.add_resource(tasks_resource.TaskListResource, '/api/tasks/<user_login>')
+    main()
