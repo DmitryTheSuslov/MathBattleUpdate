c282ddf1c768b6a1c096f52539cf5b0fc0a5b25a
Димка <mityka2004@yandex.ru>
2020-08-18 12:03:01 +0300

    Оставлен только сайт

--- api.py
+++
@@ -1,5 +0,0 @@
-import tasks_resource, users_resource
-from flask_restful import Api
-from app import app
-
-api = Api(app)
--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, url_for
-import tasks_resource, users_resource
-from flask_restful import Api
 import os
-from data import db_session
 
-db_session.global_init('db/DataBase.sqlite')
 app = Flask(__name__)
 
 
@@ -20,12 +16,4 @@ def index():
 
 
 if __name__ == '__main__':
-    from api import *
-
-
-    session = db_session.create_session()
-    api.add_resource(users_resource.UserResource, '/api/users/<user_login>')
-    api.add_resource(users_resource.UserListResource, '/api/user/')
-    api.add_resource(tasks_resource.TaskResource, '/api/task/<task_id>')
-    api.add_resource(tasks_resource.TaskListResource, '/api/tasks/<user_login>')
     main()
